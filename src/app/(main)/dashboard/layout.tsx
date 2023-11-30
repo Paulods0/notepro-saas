0ef243725a5056089386c2fb800d4109b43af772
Paulods0 <pauloluguenda0@gmail.com>
2023-11-30 12:22:36 +0100

    feat:upgrade all packages

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
   const { data: products, error } = await getActiveProductsWithPrice()
-  if (error) throw new Error("")
+
   return (
     <main className="flex overflow-hidden h-screen">
       <SubscriptionModalProvider products={products}>
